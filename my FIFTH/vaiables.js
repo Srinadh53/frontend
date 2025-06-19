4cce01e1409117b058fb4acfe1e09d25b0e2d2ef
Srinadh53 <srinadh1116@gmail.com>
2025-06-19 16:26:58 +0530

    added files

---
+++ my FIFTH/vaiables.js
@@ -0,0 +1,16 @@
+let courseName = "JavaScript";
+
+function practiceScope() {
+    // Function scope
+    let topic = "Variables";
+    console.log(courseName + " - " + topic);
+    
+    if (true) {
+        // Block scope
+        let detail = "Exploring scopes";
+        console.log(detail);
+    }
+    // console.log(detail); // Try uncommenting this
+}
+
+practiceScope();
